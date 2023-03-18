93e75adda32da4194a3798df344b2f076291daf6
Gabriel Pereyra <gabriel.pereyra@sondermut.com>
2023-03-18 17:28:07 -0600

    chore: define UI according to flow state

--- app/src/main/java/com/pereyrarg11/todolist/addtask/ui/TasksViewModel.kt
+++ app/src/main/java/com/pereyrarg11/todolist/addtask/ui/TasksViewModel.kt
@@ -1,6 +1,5 @@
 package com.pereyrarg11.todolist.addtask.ui
 
-import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -9,10 +8,12 @@ import com.pereyrarg11.todolist.addtask.domain.AddTaskUseCase
 import com.pereyrarg11.todolist.addtask.domain.GetTaskListUseCase
 import com.pereyrarg11.todolist.addtask.ui.TasksUiState.*
 import com.pereyrarg11.todolist.addtask.ui.model.TaskModel
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+@HiltViewModel
 class TasksViewModel @Inject constructor(
     private val addTaskUseCase: AddTaskUseCase,
     getTaskListUseCase: GetTaskListUseCase
@@ -30,19 +31,14 @@ class TasksViewModel @Inject constructor(
     private val _isDialogVisible = MutableLiveData<Boolean>()
     val isDialogVisible: LiveData<Boolean> = _isDialogVisible
 
-    private val _taskList = mutableStateListOf<TaskModel>()
-    val taskList: List<TaskModel> = _taskList
-
     fun closeDialog() {
         _isDialogVisible.value = false
     }
 
     fun createNewTask(taskTitle: String) {
         closeDialog()
-        val model = TaskModel(title = taskTitle)
-        _taskList.add(model)
         viewModelScope.launch {
-            addTaskUseCase(model)
+            addTaskUseCase(TaskModel(title = taskTitle))
         }
     }
 
@@ -51,14 +47,8 @@ class TasksViewModel @Inject constructor(
     }
 
     fun onTaskSelected(entity: TaskModel) {
-        val index = _taskList.indexOf(entity)
-        _taskList[index] = _taskList[index].let {
-            it.copy(selected = !it.selected)
-        }
     }
 
     fun removeItem(entity: TaskModel) {
-        val targetTask = _taskList.find { it.id == entity.id }
-        _taskList.remove(targetTask)
     }
 }
